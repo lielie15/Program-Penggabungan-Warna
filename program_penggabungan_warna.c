7d51d50ab54912e710a4dbc07511d428b11ccff3
lielie15 <lilidwihhh@gmail.com>
2025-05-19 11:40:50 +0700

    Update program_penggabungan_warna.c

--- program_penggabungan_warna.c
+++ program_penggabungan_warna.c
@@ -27,80 +27,70 @@ const char* gabungWarna(const char *warna1, const char *warna2) {
     } else if ((strcmp(warna1, "kuning") == 0 && strcmp(warna2, "putih") == 0) || (strcmp(warna1, "putih") == 0 && strcmp(warna2, "kuning") == 0)) {
         return"\033[38;2;255;255;127mKuning Muda\033[0m";  
 
-    } else if ((strcmp(warna1, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0 && strcmp(warna2, "merah") == 0) || (strcmp(warna1, "merah") == 0 && strcmp(warna2, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0)) {
-        return"033[38;2;255;0;128mMagenta\033[0m"; 
+        /////
 
-    } else if ((strcmp(warna1, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0 && strcmp(warna2, "kuning") == 0) || (strcmp(warna1, "kuning") == 0 && strcmp(warna2, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0)) {
-        return"\033[38;2;255;165;0mJingga\033[0m"; 
+    } else if ((strcmp(warna1, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;165;0mJingga\033[0m") == 0) || (strcmp(warna1, "\033[38;2;255;165;0mJingga\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0)) {
+        return"\033[38;2;255;179;102mPeach Muda\033[0m"; 
 
-    } else if ((strcmp(warna1, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0 && strcmp(warna2, "biru") == 0) || (strcmp(warna1, "biru") == 0 && strcmp(warna2, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0)) {
-        return"\033[38;2;200;162;200mUngu Muda\033[0m"; 
+    } else if ((strcmp(warna1, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0 && strcmp(warna2, "\033[1;32mHijau\033[0m") == 0) || (strcmp(warna1, "\033[1;32mHijau\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0)) {
+        return"\033[38;2;128;160;102mZaitun Pastel\033[0m"; 
 
-    } else if ((strcmp(warna1, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0 && strcmp(warna2, "putih") == 0) || (strcmp(warna1, "putih") == 0 && strcmp(warna2, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0)) {
-        return"\033[38;2;255;180;217mMerah Muda Pucat\033[0m"; 
+    } else if ((strcmp(warna1, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0 && strcmp(warna2, "\033[1;35mUngu\033[0m") == 0) || (strcmp(warna1, "\033[1;35mUngu\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0)) {
+        return"\033[38;2;192;96;166mUngu Muda Cerah\033[0m"; 
 
-    } else if ((strcmp(warna1, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0 && strcmp(warna2, "ungu") == 0) || (strcmp(warna1, "ungu") == 0 && strcmp(warna2, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0)) {
-        return"\033[38;2;139;0;0mMerah Tua\033[0m"; 
+    } else if ((strcmp(warna1, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0 && strcmp(warna2, "\033[1;36mBiru Muda\033[0m") == 0) || (strcmp(warna1, "\033[1;36mBiru Muda\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0)) {
+        return"\033[38;2;214;204;217mLavender Pastel\033[0m"; 
 
-    } else if ((strcmp(warna1, "\033[38;2;255;165;0mJingga\033[0m") == 0 && strcmp(warna2, "merah") == 0) || (strcmp(warna1, "merah") == 0 && strcmp(warna2, "\033[38;2;255;165;0mJingga\033[0m") == 0)) {
-        return"\033[38;2;255;69;0mMerah Jingga\033[0m";
+    } else if ((strcmp(warna1, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0) || (strcmp(warna1, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0)) {
+        return"\033[38;2;255;224;153mPeach Pastel\033[0m";
 
-    } else if ((strcmp(warna1, "\033[38;2;255;165;0mJingga\033[0m") == 0 && strcmp(warna2, "kuning") == 0) || (strcmp(warna1, "kuning") == 0 && strcmp(warna2, "\033[38;2;255;165;0mJingga\033[0m") == 0)) {
-        return"\033[38;2;255;200;0mKuning Jingga\033[0m";
+    } else if ((strcmp(warna1, "\033[38;2;255;165;0mJingga\033[0m") == 0 && strcmp(warna2, "\033[1;32mHijau\033[0m") == 0) || (strcmp(warna1, "\033[1;32mHijau\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;165;0mJingga\033[0m") == 0)) {
+        return"\033[38;2;127;147;0mHijau Limau Tua\033[0m"; 
 
-    } else if ((strcmp(warna1, "\033[38;2;255;165;0mJingga\033[0m") == 0 && strcmp(warna2, "biru") == 0) || (strcmp(warna1, "biru") == 0 && strcmp(warna2, "\033[38;2;255;165;0mJingga\033[0m") == 0)) {
-        return"\033[38;2;128;82;128mCokelat Kemerahan\033[0m";
+    } else if ((strcmp(warna1, "\033[38;2;255;165;0mJingga\033[0m") == 0 && strcmp(warna2, "\033[1;35mUngu\033[0m") == 0) || (strcmp(warna1, "\033[1;35mUngu\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;165;0mJingga\033[0m") == 0)) {
+        return"\033[38;2;192;83;64mCokelat Merah Tembaga\033[0m";
 
-    } else if ((strcmp(warna1, "\033[38;2;255;165;0mJingga\033[0m") == 0 && strcmp(warna2, "putih") == 0) || (strcmp(warna1, "putih") == 0 && strcmp(warna2, "\033[38;2;255;165;0mJingga\033[0m") == 0)) {
-        return"\033[38;2;255;210;128mKrem\033[0m";
+    } else if ((strcmp(warna1, "\033[38;2;255;165;0mJingga\033[0m") == 0 && strcmp(warna2, "\033[1;36mBiru Muda\033[0m") == 0) || (strcmp(warna1, "\033[1;36mBiru Muda\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;165;0mJingga\033[0m") == 0)) {
+        return"\033[38;2;214;191;115mKaramel Pastel\033[0m";
+
+    } else if ((strcmp(warna1, "\033[38;2;255;165;0mJingga\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0) || (strcmp(warna1, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;165;0mJingga\033[0m") == 0)) {
+        return"\033[38;2;255;210;51mKuning Jingga Cerah\033[0m";
+
+    } else if ((strcmp(warna1, "\033[1;32mHijau\033[0m") == 0 && strcmp(warna2, "\033[1;35mUngu\033[0m") == 0) || (strcmp(warna1, "\033[1;35mUngu\033[0m") == 0 && strcmp(warna2, "\033[1;32mHijau\033[0m") == 0)) {
+        return"\033[38;2;64;64;64mAbu-abu Gelap\033[0m";
         
-    } else if ((strcmp(warna1, "\033[1;32mHijau\033[0m") == 0 && strcmp(warna2, "putih") == 0) || (strcmp(warna1, "putih") == 0 && strcmp(warna2, "\033[1;32mHijau\033[0m") == 0)) {
-        return"\033[38;2;144;238;144mHijau Muda\033[0m";
+    } else if ((strcmp(warna1, "\033[1;32mHijau\033[0m") == 0 && strcmp(warna2, "\033[1;36mBiru Muda\033[0m") == 0) || (strcmp(warna1, "\033[1;36mBiru Muda\033[0m") == 0 && strcmp(warna2, "\033[1;32mHijau\033[0m") == 0)) {
+        return"\033[38;2;87;172;115mHijau Laut Pastel\033[0m";
 
-    } else if ((strcmp(warna1, "\033[1;32mHijau\033[0m") == 0 && strcmp(warna2, "biru") == 0) || (strcmp(warna1, "biru") == 0 && strcmp(warna2, "\033[1;32mHijau\033[0m") == 0)) {
-        return"\033[38;2;0;128;128mHijau Toska\033[0m";
+    } else if ((strcmp(warna1, "\033[1;32mHijau\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0) || (strcmp(warna1, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0 && strcmp(warna2, "\033[1;32mHijau\033[0m") == 0)) {
+        return"\033[38;2;128;192;51mHijau Limau Cerah\033[0m";
 
-    } else if ((strcmp(warna1, "\033[1;32mHijau\033[0m") == 0 && strcmp(warna2, "kuning") == 0) || (strcmp(warna1, "kuning") == 0 && strcmp(warna2, "\033[1;32mHijau\033[0m") == 0)) {
-        return"\033[38;2;127;191;0mHijau Limau\033[0m";
+    } else if ((strcmp(warna1, "\033[1;35mUngu\033[0m") == 0 && strcmp(warna2, "\033[1;36mBiru Muda\033[0m") == 0) || (strcmp(warna1, "\033[1;36mBiru Muda\033[0m") == 0 && strcmp(warna2, "\033[1;35mUngu\033[0m") == 0)) {
+        return"\033[38;2;151;108;179mLavender Kebiruan\033[0m";
 
-    } else if ((strcmp(warna1, "\033[1;32mHijau\033[0m") == 0 && strcmp(warna2, "merah") == 0) || (strcmp(warna1, "merah") == 0 && strcmp(warna2, "\033[1;32mHijau\033[0m") == 0)) {
-        return"\033[38;2;204;204;0mCokelat\033[0m";
+    } else if ((strcmp(warna1, "\033[1;35mUngu\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0) || (strcmp(warna1, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0 && strcmp(warna2, "\033[1;35mUngu\033[0m") == 0)) {
+        return"\033[38;2;192;128;115mSalmon Tua\033[0m";
 
-    } else if ((strcmp(warna1, "\033[1;35mUngu\033[0m") == 0 && strcmp(warna2, "merah") == 0) || (strcmp(warna1, "merah") == 0 && strcmp(warna2, "\033[1;35mUngu\033[0m") == 0)) {
-        return"\033[38;2;191;0;64mMagenta\033[0m";
+    } else if ((strcmp(warna1, "\033[1;36mBiru Muda\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0) || (strcmp(warna1, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0 && strcmp(warna2, "\033[1;36mBiru Muda\033[0m") == 0)) {
+        return"\033[38;2;214;236;166mHijau Lemon Pastel\033[0m";
 
-    } else if ((strcmp(warna1, "\033[1;35mUngu\033[0m") == 0 && strcmp(warna2, "kuning") == 0) || (strcmp(warna1, "kuning") == 0 && strcmp(warna2, "\033[1;35mUngu\033[0m") == 0)) {
-        return"\033[38;2;139;69;19mCokelat\033[0m";
-
-    } else if ((strcmp(warna1, "\033[1;35mUngu\033[0m") == 0 && strcmp(warna2, "biru") == 0) || (strcmp(warna1, "biru") == 0 && strcmp(warna2, "\033[1;35mUngu\033[0m") == 0)) {
-        return"\033[38;2;64;0;192mBiru Tua\033[0m";
-
-    } else if ((strcmp(warna1, "\033[1;35mUngu\033[0m") == 0 && strcmp(warna2, "putih") == 0) || (strcmp(warna1, "putih") == 0 && strcmp(warna2, "\033[1;35mUngu\033[0m") == 0)) {
+    } else if ((strcmp(warna1, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0) || (strcmp(warna1, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;105;180mMerah Muda\033[0m") == 0)) {
         return"\033[38;2;200;162;200mUngu Muda\033[0m";
 
-    } else if ((strcmp(warna1, "\033[1;36mBiru Muda\033[0m") == 0 && strcmp(warna2, "putih") == 0) || (strcmp(warna1, "putih") == 0 && strcmp(warna2, "\033[1;36mBiru Muda\033[0m") == 0)) {
-        return"\033[38;2;173;216;230mBiru Sangat Muda\033[0m";
+    } else if ((strcmp(warna1, "\033[38;2;255;165;0mJingga\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;165;0mJingga\033[0m") == 0) || (strcmp(warna1, "\033[38;2;255;165;0mJingga\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;165;0mJingga\033[0m") == 0)) {
+        return"\033[38;2;173;216;230mBiru Muda terang\033[0m";
 
-    } else if ((strcmp(warna1, "\033[1;36mBiru Muda\033[0m") == 0 && strcmp(warna2, "biru") == 0) || (strcmp(warna1, "biru") == 0 && strcmp(warna2, "\033[1;36mBiru Muda\033[0m") == 0)) {
-        return"\033[1;34mBiru\033[0m\n";
+    } else if ((strcmp(warna1, "\033[1;32mHijau\033[0m") == 0 && strcmp(warna2, "\033[1;32mHijau\033[0m") == 0) || (strcmp(warna1, "\033[1;32mHijau\033[0m") == 0 && strcmp(warna2, "\033[1;32mHijau\033[0m") == 0)) {
+        return"\033[1;32mHijau\033[0m";
 
-    } else if ((strcmp(warna1, "\033[1;36mBiru Muda\033[0m") == 0 && strcmp(warna2, "kuning") == 0) || (strcmp(warna1, "kuning") == 0 && strcmp(warna2, "\033[1;36mBiru Muda\033[0m") == 0)) {
-        return"\033[38;2;144;238;144mHijau Muda\033[0m";
+    } else if ((strcmp(warna1, "\033[1;35mUngu\033[0m") == 0 && strcmp(warna2, "\033[1;35mUngu\033[0m") == 0) || (strcmp(warna1, "\033[1;35mUngu\033[0m") == 0 && strcmp(warna2, "\033[1;35mUngu\033[0m") == 0)) {
+        return"\033[1;35mUngu\033[0m";
 
-    } else if ((strcmp(warna1, "\033[1;36mBiru Muda\033[0m") == 0 && strcmp(warna2, "merah") == 0) || (strcmp(warna1, "merah") == 0 && strcmp(warna2, "\033[1;36mBiru Muda\033[0m") == 0)) {
-        return"\033[38;2;200;162;200mUngu Muda\033[0m";
+    } else if ((strcmp(warna1, "\033[1;36mBiru Muda\033[0m") == 0 && strcmp(warna2, "\033[1;36mBiru Muda\033[0m") == 0) || (strcmp(warna1, "\033[1;36mBiru Muda\033[0m") == 0 && strcmp(warna2, "\033[1;36mBiru Muda\033[0m") == 0)) {
+        return"\033[1;36mBiru Muda\033[0m";
 
-    } else if ((strcmp(warna1, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0 && strcmp(warna2, "merah") == 0) || (strcmp(warna1, "merah") == 0 && strcmp(warna2, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0)) {
-        return"\033[38;2;255;165;0mJingga\033[0m";
-
-    } else if ((strcmp(warna1, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0 && strcmp(warna2, "kuning") == 0) || (strcmp(warna1, "kuning") == 0 && strcmp(warna2, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0)) {
-        return"\033[38;2;255;223;0mKuning Cerah\033[0m";
-
-    } else if ((strcmp(warna1, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0 && strcmp(warna2, "biru") == 0) || (strcmp(warna1, "biru") == 0 && strcmp(warna2, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0)) {
-        return"\033[38;2;144;238;144mHijau Muda\033[0m";
-
-    } else if ((strcmp(warna1, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0 && strcmp(warna2, "putih") == 0) || (strcmp(warna1, "putih") == 0 && strcmp(warna2, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0)) {
-        return"\033[38;2;255;210;128mKrem\033[0m";
+    } else if ((strcmp(warna1, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0) || (strcmp(warna1, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0 && strcmp(warna2, "\033[38;2;255;255;127mKuning Muda\033[0m") == 0)) {
+        return"\033[38;2;255;255;127mKuning Muda\033[0m";
 
     } else {
         return"Tidak diketahui";
@@ -110,6 +100,8 @@ const char* gabungWarna(const char *warna1, const char *warna2) {
 int main() {
     char warna1[20];
     char warna2[20];
+    char warna3[20];
+    char warna4[20];
     char is_gabung[2]; 
 
     printf("\nPlihlah Warna!\n");
@@ -119,6 +111,8 @@ int main() {
     printf("\033[1;34mBiru\033[0m\n");
     printf("\033[1;37mPutih\033[0m\n\n");
 
+    printf("--------------------------------\n");
+
     printf("Masukkan warna ke-1 : ");
     scanf("%s", warna1);
     toLowerStr(warna1);
@@ -127,22 +121,32 @@ int main() {
     scanf("%s", warna2);
     toLowerStr(warna2);
 
-
     const char* hasil = gabungWarna(warna1, warna2);
     printf("\nHasil penggabungan antara warna %s dan warna %s yaitu menjadi warna %s\n\n", warna1, warna2, hasil);
 
-    if (strcmp(hasil, "Tidak diketahui") != 0) {
-        printf("Apakah %s ingin digabungkan dengan warna lain? (y/n) : ", hasil);
+    printf("--------------------------------\n");
+
+    printf("Masukkan warna ke-3 : ");
+    scanf("%s", warna3);
+    toLowerStr(warna3);
+
+    printf("Masukkan warna ke-4 : ");
+    scanf("%s", warna4);
+    toLowerStr(warna4);
+
+    const char* hasil2 = gabungWarna(warna3, warna4);
+    printf("\nHasil penggabungan antara warna %s dan warna %s yaitu menjadi warna %s\n\n", warna3, warna4, hasil2);
+
+    if (strcmp(hasil, "Tidak diketahui") != 0 && strcmp(hasil2, "Tidak diketahui") != 0) {
+        printf("Apakah warna %s ingin digabungkan dengan warna %s? (y/n) : ", hasil, hasil2);
         scanf("%s", is_gabung);
 
         if(strcmp(is_gabung, "y") == 0){
-            printf("\nWarna ke-1 : %s\n", hasil);
-            printf("Masukkan warna ke-2 : ");
-            scanf("%s", warna2);
-            toLowerStr(warna2);
+            printf("\nWarna ke-1 : %s", hasil);
+            printf("\nWarna ke-2 : %s\n", hasil2);
 
-            const char* hasil_next = gabungWarna(hasil, warna2);
-            printf("\nHasil penggabungan antara warna %s dan warna %s yaitu menjadi warna %s\n\n", hasil, warna2, hasil_next);
+            const char* hasil_next = gabungWarna(hasil, hasil2);
+            printf("\nHasil penggabungan antara warna %s dan warna %s yaitu menjadi warna %s\n\n", hasil, hasil2, hasil_next);
 
             printf("Apakah ingin mencoba penggabungan warna lainnya? (y/n) : ");
             scanf("%s", is_gabung);
@@ -163,6 +167,7 @@ int main() {
             }        
         }
     } else {
+        printf("Kedua hasil warna tidak dapat digabungkan, karena salah satu atau kedua warnanya tidak diketahui\n\n");
         printf("Apakah ingin mencoba penggabungan warna lainnya? (y/n) : ");
         scanf("%s", is_gabung);
 
